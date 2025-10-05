2f58f5b5f28cd75f9c185d719c573f9461fca746
Ernest Zduńczyk <81569790+Nixir-hub@users.noreply.github.com>
2025-10-05 15:51:24 +0200

    Update settings.py

--- Bookin_conference_room_app/settings.py
+++ Bookin_conference_room_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--)fjx2u%3y&#9^e2341edkbh2$lced2-j&a$po5dkf449&u=)*'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
